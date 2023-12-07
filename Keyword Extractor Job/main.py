e19f58f53974c819d4ae90c16a6933e94ec98fa1
Merlin Carter <merlin+demo@quix.io>
2023-12-07 12:54:37 +0000

    Edited 'main.py'

--- Keyword Extractor Job/main.py
+++ Keyword Extractor Job/main.py
@@ -2,13 +2,13 @@ import requests
 import zipfile
 import gdown
 
-url = 'https://drive.google.com/uc?id=1eIdeNAOe40JN3Ogz7okoGuW_h7ToYjyj'
+# url = 'https://drive.google.com/uc?id=1eIdeNAOe40JN3Ogz7okoGuW_h7ToYjyj'
 #url = 'https://drive.google.com/file/d/1eIdeNAOe40JN3Ogz7okoGuW_h7ToYjyj/view?usp=drive_link'
-output = 'r_dataengineering.zip'
-gdown.download(url, output, quiet=False)
+# output = 'r_dataengineering.zip'
+# gdown.download(url, output, quiet=False)
 
 # Unzip the file
-# with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-#     zip_ref.extractall(extract_path)
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+     zip_ref.extractall(extract_path)
 
-# print('Download and extraction complete.')
+print('Download and extraction complete.')
