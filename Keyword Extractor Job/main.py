3029795cd35efe7409b3272d48c9e5d4ebb28ea5
Merlin Carter <merlin+demo@quix.io>
2023-12-07 12:53:21 +0000

    Edited 'main.py'

--- Keyword Extractor Job/main.py
+++ Keyword Extractor Job/main.py
@@ -1,31 +1,11 @@
 import requests
 import zipfile
+import gdown
 
-# Direct download link for the Google Drive file
-direct_link = 'https://drive.google.com/file/d/1eIdeNAOe40JN3Ogz7okoGuW_h7ToYjyj/view?usp=sharing'
-
-# Path to save the downloaded zip file
-zip_file_path = 'r_dataengineering.zip'
-
-# Path to extract the contents of the zip file
-extract_path = '.'
-
-# Stream the download (download in chunks)
-response = requests.get(direct_link, stream=True)
-response.raise_for_status()  # Ensure the download was successful
-
-chunk_size = 1024  # 1 kilobyte
-total_downloaded = 0
-
-# Open the file to write as binary - 'wb'
-with open(zip_file_path, 'wb') as file:
-    for chunk in response.iter_content(chunk_size):
-        # Write the chunk to the file
-        file.write(chunk)
-        # Update the total number of bytes downloaded
-        total_downloaded += len(chunk)
-        # Print the progress
-        print(f'Downloaded {total_downloaded} bytes', end='\r')
+url = 'https://drive.google.com/uc?id=0B9P1L--7Wd2vNm9zMTJWOGxobkU'
+url = 'https://drive.google.com/file/d/1eIdeNAOe40JN3Ogz7okoGuW_h7ToYjyj/view?usp=drive_link'
+output = 'r_dataengineering.zip'
+gdown.download(url, output, quiet=False)
 
 # Unzip the file
 # with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
