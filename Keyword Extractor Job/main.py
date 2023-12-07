b51dffe258ece06c3c151f038accd7c0680c7886
Merlin Carter <merlin+demo@quix.io>
2023-12-07 12:33:21 +0000

    Edited 'main.py'

--- Keyword Extractor Job/main.py
+++ Keyword Extractor Job/main.py
@@ -18,9 +18,6 @@ client = qx.QuixStreamingClient()
 # Open the output topic where to write data out
 topic_producer = client.get_topic_producer(topic_id_or_name = os.environ["output"])
 
-# Set stream ID or leave parameters empty to get stream ID generated.
-stream = topic_producer.get_or_create_stream()
-stream.properties.name = "Hello World Python stream"
 
 def extract_keywords_with_error_handling(doc):
     try:
@@ -33,7 +30,8 @@ def extract_keywords_with_error_handling(doc):
 def process_and_send(row):
     # Process the row
     row_basic = row[['created_utc', 'parent_id', 'author', 'body']].copy()
-    row_basic['human_readable_time'] = pd.to_datetime(row_basic['created_utc'], unit='s')
+    row_basic  = row_basic .rename(columns={"created_utc": "timestamp"})
+    row_basic['human_timestamp'] = pd.to_datetime(row_basic['timestamp'], unit='s')
     row_basic['word_count'] = row_basic['body'].str.split().apply(len)
     row_basic['extracted_keywords'] = extract_keywords_with_error_handling(row_basic['body'])
     
@@ -42,7 +40,12 @@ def process_and_send(row):
     processed_data['process_id'] = os.getpid()
     
     # Send processed data to Kafka
-    producer.send('your_topic_name', processed_data)
+    # Set stream ID or leave parameters empty to get stream ID generated.
+    stream_producer = topic_producer.get_or_create_stream(row_basic[])
+    stream_producer.properties.name = "Keyword Extraction"
+
+     # publish the data to the Quix stream created earlier
+    stream_producer.timeseries.publish(row_basic)
 
 # Read the JSONL file and process each line
 with open('r_dataengineering_comments.jsonl', 'r', encoding='utf-8') as file:
@@ -52,3 +55,5 @@ with open('r_dataengineering_comments.jsonl', 'r', encoding='utf-8') as file:
         process_and_send(df_row)
 
 # Ensure all messages are sent before closing the producer
+print("Closing stream")
+stream.close()
