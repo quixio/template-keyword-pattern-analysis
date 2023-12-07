9366a1c01ee2745b7b88cd50acb90869cc381122
Merlin Carter <merlin+demo@quix.io>
2023-12-07 12:23:51 +0000

    Edited 'download_zip.py'

---
+++ Keyword Extractor Job/download_zip.py
@@ -0,0 +1,41 @@
+import requests
+import zipfile
+import io
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://drive.google.com/uc?export=download"
+
+    session = requests.Session()
+    response = session.get(URL, params={'id': id}, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = {'id': id, 'confirm': token}
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, destination)
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+
+# Google Drive ID of the file
+file_id = 'YOUR_FILE_ID'
+# Destination where you want to save the zip file
+destination = 'path/to/your/file.zip'
+
+download_file_from_google_drive(file_id, destination)
+
+# Unzip the file
+with zipfile.ZipFile(destination, 'r') as zip_ref:
+    zip_ref.extractall('path/to/extract/directory')
