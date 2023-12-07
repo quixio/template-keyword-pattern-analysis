0a16bf6cbbe282981bc7be60ac00fc6a802fae1c
Merlin Carter <merlin+demo@quix.io>
2023-12-07 12:45:26 +0000

    Edited 'main.py'

--- Keyword Extractor Job/main.py
+++ Keyword Extractor Job/main.py
@@ -1,40 +1,23 @@
 import requests
 import zipfile
 
-def download_file_from_google_drive(id, destination):
-    URL = "https://drive.google.com/uc?export=download"
+# Direct download link for the Google Drive file
+direct_link = 'https://drive.google.com/uc?id=1eIdeNAOe40JN3Ogz7okoGuW_h7ToYjyj&export=download'
 
-    session = requests.Session()
-    response = session.get(URL, params={'id': id}, stream=True)
-    token = get_confirm_token(response)
+# Path to save the downloaded zip file
+zip_file_path = 'r_dataengineering.zip'
 
-    if token:
-        params = {'id': id, 'confirm': token}
-        response = session.get(URL, params=params, stream=True)
+# Path to extract the contents of the zip file
+extract_path = '.'
 
-    save_response_content(response, destination)
+# Download the file
+response = requests.get(direct_link)
+response.raise_for_status()  # Ensure the download was successful
 
-def get_confirm_token(response):
-    for key, value in response.cookies.items():
-        if key.startswith('download_warning'):
-            return value
-    return None
-
-def save_response_content(response, destination):
-    CHUNK_SIZE = 32768
-
-    with open(destination, "wb") as f:
-        for chunk in response.iter_content(CHUNK_SIZE):
-            if chunk:  # filter out keep-alive new chunks
-                f.write(chunk)
-
-# Google Drive ID of the file
-file_id = '1eIdeNAOe40JN3Ogz7okoGuW_h7ToYjyj'
-# Destination where you want to save the zip file
-destination = 'r_dataengineering.zip'
-
-download_file_from_google_drive(file_id, destination)
+# Save the file to storage
+with open(zip_file_path, 'wb') as file:
+    file.write(response.content)
 
 # Unzip the file
-#with zipfile.ZipFile(destination, 'r') as zip_ref:
-#    zip_ref.extractall('.')
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    zip_ref.extractall(extract_path)
