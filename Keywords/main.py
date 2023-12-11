aadc018813ee061d457131d0d709b61c7ff844e5
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-11 18:36:48 +0000

    Edited 'main.py'

--- Keywords/main.py
+++ Keywords/main.py
@@ -50,7 +50,7 @@ def publish(keyword_data):
     for keyword, data in keyword_data.items():
         print(f"Keyword: {keyword}, Count: {data['count']}, Total Score: {data['total_score']}")
         output_sdf["blah"] = 100
-        print(output_sdf["blah"])
+        print(output_sdf["blah"][0])
 
 sdf = sdf.apply(reply)
 
