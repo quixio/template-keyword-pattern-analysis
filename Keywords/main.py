981c962d4242c65c2f63970945388ff3c7cda79a
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-11 17:16:14 +0000

    Edited 'main.py'

--- Keywords/main.py
+++ Keywords/main.py
@@ -2,6 +2,8 @@ from quixstreams import Application, State
 from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
 import os
 import time
+import ast
+
 
 app = Application.Quix("keywords-1", auto_offset_reset="latest")
 input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
@@ -14,22 +16,13 @@ keyword_data = {}
 
 def reply(row: dict):
     global keyword_data
+
+    # Convert the 'extracted_keywords' field from a string to a list of tuples
+    data = ast.literal_eval(row['extracted_keywords'])
+
     print("---")
-    print(row)
+    print(data)
     print("---")
-
-    # Check if the row has an 'extracted_keywords' field
-    if 'extracted_keywords' not in row:
-        print(f"Warning: row does not have an 'extracted_keywords' field: {row}")
-        return
-
-    data = row['extracted_keywords']
-    #print(data)
-
-    # Check if data is a list of tuples
-    if not isinstance(data, list) or not all(isinstance(item, tuple) and len(item) == 2 for item in data):
-        #print(f"Warning: 'extracted_keywords' field is not a list of tuples: {data}")
-        return
 
     # Process the data
     for keyword, score in data:
