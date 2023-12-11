7d98df80a76a77667f95da4b33d0a1a1acc12abf
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-11 16:54:35 +0000

    Edited 'main.py'

--- Keywords/main.py
+++ Keywords/main.py
@@ -1,5 +1,22 @@
-import quixstreams as qx
+from quixstreams import Application, State
+from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer
 import os
-import pandas as pd
+import time
 
+app = Application.Quix("keywords-1", auto_offset_reset="latest")
+input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
+output_topic = app.topic(os.environ["output"], value_serializer=QuixTimeseriesSerializer())
 
+sdf = app.dataframe(input_topic)
+
+def reply(row: dict, state: State):
+    print(row)
+
+sdf = sdf.apply(reply, stateful=True)
+
+sdf["Timestamp"] = sdf["Timestamp"].apply(lambda row: time.time_ns())
+
+sdf = sdf.to_topic(output_topic)
+
+if __name__ == "__main__":
+    app.run(sdf)
