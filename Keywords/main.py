7ece15c4815f7d15ab7e68b3f0c239a6bd5fbaa8
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-12 16:42:12 +0000

    Edited 'main.py'

--- Keywords/main.py
+++ Keywords/main.py
@@ -138,7 +138,7 @@ def sdf_way():
     # sdf = sdf.update(lambda row: print(row))
 
     # publish to output topic
-    sdf = sdf.to_topic(output_topic)
+    #sdf = sdf.to_topic(output_topic)
     return sdf
 
 def old_way():
