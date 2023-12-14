654f29e1e507b90d5f30a70cb62ada77795e2152
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-14 15:06:09 +0000

    Edited 'main.py'

--- KeywordCount/main.py
+++ KeywordCount/main.py
@@ -21,84 +21,6 @@ input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer
 output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer())
 
 
-def process_rows(row: dict, state: State):
-
-    # Convert the 'extracted_keywords' field from a string to a list of tuples
-    if 'extracted_keywords' not in row or row['extracted_keywords'] is None:
-        print(f"Warning: row does not have an 'extracted_keywords' field or it's None: {row}")
-        return
-
-    print("---------------")
-    print("extracted_keywords" in row)
-    print("---------------")
-
-    new_rows = dict(ast.literal_eval(row['extracted_keywords']))
-    new_rows['Timestamp'] = row['Timestamp']
-    
-    print("---------------")
-    print("1")
-    #print(new_rows)
-    print("---------------")
-    
-    # remove columns 
-    if 'parent_id' in new_rows:
-        del new_rows['parent_id']
-    if 'author' in new_rows:
-        del new_rows['author']
-    if 'body' in new_rows:
-        del new_rows['body']
-    if 'human_timestamp' in new_rows:
-        del new_rows['human_timestamp']
-    
-    print("---------------")
-    print("2")
-    #print(new_rows)
-    print("---------------")
-    
-    # get sums from state, init with empty {} if not there
-    sums_state = state.get("sums", {})
-    
-    print("---------------")
-    print("3")
-    print("---------------")
-    
-    # get/add to/from state and sum the values
-    for key in new_rows:
-        if key not in sums_state:
-            val = new_rows[key]
-            if isinstance(val, int):
-                sums_state[key] = 1#val
-            else:
-                print(f"bad int value {val}")
-        else:
-            val = new_rows[key]
-            if isinstance(val, int):
-                sums_state[key] += 1#val
-            else:
-                print(f"bad int value {val}")
-
-        new_rows[key] = sums_state[key]
-    
-    print("---------------")
-    print("4")
-    print("---------------")
-    
-    print("---------------")
-    print("---------------")
-    # print(sums_state)
-    print("---------------")
-    print("---------------")
-
-    # update state with the new state
-    state.set('sums', sums_state)
-    print("---------------")
-    print("5")
-    print("---------------")
-    
-    # return the new rows, which will update the sdf 
-    # (this function was called with apply)
-    return new_rows
-
 
 # expand keywords from a nested dict to rows (keeping the timestamp)
 def expand_keywords(row: dict):
@@ -164,7 +86,7 @@ def sdf_way():
 
     #sdf = sdf.update(lambda row: print(row))
 
-    # condider using....
+    # consider using....
     # Convert the string to a dictionary
     # my_dict = json.loads(value)
 
@@ -186,23 +108,7 @@ def sdf_way():
     sdf = sdf.to_topic(output_topic)
     return sdf
 
-def old_way():
-    sdf = app.dataframe(input_topic)
-
-    # do all the processing in process_rows
-    sdf = sdf.apply(process_rows, stateful=True)
-
-    # print
-    #sdf = sdf.update(lambda row: print(row))
-
-    # publish to output topic
-    #sdf = sdf.to_topic(output_topic)
-    return sdf
-
-
-# uncomment the one you want to use..
 sdf = sdf_way()
-#sdf = old_way()
 
 if __name__ == "__main__":
     app.run(sdf)
