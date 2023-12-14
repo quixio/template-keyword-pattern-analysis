c03222f628d0ba43e6d943dfdf5e2a1f5cc5b9b8
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-14 10:25:59 +0000

    Copied files from Keywords

---
+++ KeywordCount/main.py
@@ -0,0 +1,208 @@
+# this code recieves data with a column called 'extracted_keywords'
+#
+# "extracted_keywords": [
+#     "[('data engineer', 0.6018), ('software engineering skills', 0.5121), ('software engineer', 0.4633), ('software engineering principles', 0.2394), ('someone', 0.1321)]"
+# ]
+#
+# The output is the sum of the values for the extracted_keywords
+#
+
+
+
+from quixstreams import Application, State
+from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer, JSONSerializer
+import os
+import time
+import ast
+
+
+app = Application.Quix("keywords-2", auto_offset_reset="earliest")
+input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
+output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer())
+
+
+def process_rows(row: dict, state: State):
+
+    # Convert the 'extracted_keywords' field from a string to a list of tuples
+    if 'extracted_keywords' not in row or row['extracted_keywords'] is None:
+        print(f"Warning: row does not have an 'extracted_keywords' field or it's None: {row}")
+        return
+
+    print("---------------")
+    print("extracted_keywords" in row)
+    print("---------------")
+
+    new_rows = dict(ast.literal_eval(row['extracted_keywords']))
+    new_rows['Timestamp'] = row['Timestamp']
+    
+    print("---------------")
+    print("1")
+    #print(new_rows)
+    print("---------------")
+    
+    # remove columns 
+    if 'parent_id' in new_rows:
+        del new_rows['parent_id']
+    if 'author' in new_rows:
+        del new_rows['author']
+    if 'body' in new_rows:
+        del new_rows['body']
+    if 'human_timestamp' in new_rows:
+        del new_rows['human_timestamp']
+    
+    print("---------------")
+    print("2")
+    #print(new_rows)
+    print("---------------")
+    
+    # get sums from state, init with empty {} if not there
+    sums_state = state.get("sums", {})
+    
+    print("---------------")
+    print("3")
+    print("---------------")
+    
+    # get/add to/from state and sum the values
+    for key in new_rows:
+        if key not in sums_state:
+            val = new_rows[key]
+            if isinstance(val, int):
+                sums_state[key] = 1#val
+            else:
+                print(f"bad int value {val}")
+        else:
+            val = new_rows[key]
+            if isinstance(val, int):
+                sums_state[key] += 1#val
+            else:
+                print(f"bad int value {val}")
+
+        new_rows[key] = sums_state[key]
+    
+    print("---------------")
+    print("4")
+    print("---------------")
+    
+    print("---------------")
+    print("---------------")
+    # print(sums_state)
+    print("---------------")
+    print("---------------")
+
+    # update state with the new state
+    state.set('sums', sums_state)
+    print("---------------")
+    print("5")
+    print("---------------")
+    
+    # return the new rows, which will update the sdf 
+    # (this function was called with apply)
+    return new_rows
+
+
+# expand keywords from a nested dict to rows (keeping the timestamp)
+def expand_keywords(row: dict):
+    new_rows = row['extracted_keywords']
+
+    # we need the timestamp, otherwise we could use sdf's expand function
+    new_rows['Timestamp'] = row['Timestamp']
+    return new_rows
+
+clear_state = True
+def sum_keywords(row: dict, state: State):
+    global clear_state
+
+
+    print("-1-")
+    print(row)
+
+    if clear_state:
+        state.set("sums", {})
+        clear_state = False
+
+    sums_state = state.get("sums", {})
+    
+    print("-2-")
+    print(sums_state)
+
+    for key in row:
+        print("-2a-")
+        print(key)
+
+        if key not in sums_state or key == "Timestamp":
+            print("-2b-")
+            sums_state[key] = row[key]
+        else:
+            print("-2c-")
+            sums_state[key] += row[key]
+        
+        # if key == "Timestamp":
+        #     sums_state[key] = row[key]
+
+        print("-2d-")
+        print(row[key])
+        row[key] = sums_state[key]
+    
+    print("-3-")
+    print(sums_state)
+
+    state.set('sums', sums_state)
+    time.sleep(0.3)
+
+    return row
+    #return sums_state
+
+
+def sdf_way():
+    sdf = app.dataframe(input_topic)
+
+    # filter data
+    sdf = sdf[sdf.contains('extracted_keywords')]
+    sdf = sdf[sdf['extracted_keywords'].notnull()]
+
+    # parse extracted keyword column (change string to dict)
+
+    #sdf = sdf.update(lambda row: print(row))
+
+    # condider using....
+    # Convert the string to a dictionary
+    # my_dict = json.loads(value)
+
+    sdf['extracted_keywords'] = sdf['extracted_keywords'].apply(lambda value: dict(ast.literal_eval(value)))
+    # sdf = sdf.update(lambda row: print(row))
+
+    # expand keywords from a nested dict to rows (keeping the timestamp)
+    sdf = sdf.apply(expand_keywords)
+
+    # sum keywords and save to state
+    sdf = sdf.apply(sum_keywords, stateful=True)
+
+    # print
+    print("====")
+    sdf = sdf.update(lambda row: print(f"&&&&&&&&&&&&&&&{row}&&&&&&&&&&&&&&&&"))
+    print("====")
+
+    # publish to output topic
+    sdf = sdf.to_topic(output_topic)
+    return sdf
+
+def old_way():
+    sdf = app.dataframe(input_topic)
+
+    # do all the processing in process_rows
+    sdf = sdf.apply(process_rows, stateful=True)
+
+    # print
+    #sdf = sdf.update(lambda row: print(row))
+
+    # publish to output topic
+    #sdf = sdf.to_topic(output_topic)
+    return sdf
+
+
+# uncomment the one you want to use..
+sdf = sdf_way()
+#sdf = old_way()
+
+if __name__ == "__main__":
+    app.run(sdf)
