7b7e647a125ae764c34e080ce5df21db9ac80d3f
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-15 09:22:34 +0000

    Edited 'main.py'

--- KeywordCountV2/main.py
+++ KeywordCountV2/main.py
@@ -4,6 +4,8 @@ import pandas as pd
 from collections import defaultdict
 from datetime import datetime, timedelta
 import ast
+import signal
+
 
 client = qx.QuixStreamingClient()
 
@@ -14,29 +16,35 @@ topic_producer = client.get_topic_producer(os.environ["output"])
 keyword_counts = defaultdict(lambda: defaultdict(list))
 
 def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-    # Extract keywords and timestamp from the DataFrame
-    keywords = ast.literal_eval(df['StringValues']['extracted_keywords'][0])
-    timestamp = pd.to_datetime(df['Timestamps'][0], unit='ns')
+    
+    try:
+        # Extract keywords and timestamp from the DataFrame
+        keywords = ast.literal_eval(df['StringValues']['extracted_keywords'][0])
+        timestamp = pd.to_datetime(df['Timestamps'][0], unit='ns')
 
-    # Update keyword counts and timestamps
-    for keyword, _ in keywords:
-        keyword_counts[keyword]['timestamps'].append(timestamp)
-        #keyword_counts[keyword]['counts'].append(len(keyword_counts[keyword]['timestamps']))
+        # Update keyword counts and timestamps
+        for keyword, _ in keywords:
+            keyword_counts[keyword]['timestamps'].append(timestamp)
+            #keyword_counts[keyword]['counts'].append(len(keyword_counts[keyword]['timestamps']))
 
-    # Calculate and print keyword counts in the specified time periods
-    for keyword, data in keyword_counts.items():
-        timestamps = data['timestamps']
-        #counts = data['counts']
+        # Calculate and print keyword counts in the specified time periods
+        for keyword, data in keyword_counts.items():
+            timestamps = data['timestamps']
+            #counts = data['counts']
 
-        for period in [1, 15, 60]:  # Time periods in minutes
-            period_start = timestamp - timedelta(minutes=period)
-            count = sum(t >= period_start for t in timestamps)
-            print(f"Count of '{keyword}' in the last {period} minutes: {count}")
+            for period in [1, 15, 60]:  # Time periods in minutes
+                period_start = timestamp - timedelta(minutes=period)
+                count = sum(t >= period_start for t in timestamps)
+                print(f"Count of '{keyword}' in the last {period} minutes: {count}")
 
-    # Continue with the existing code...
-    #stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-    #stream_producer.timeseries.buffer.publish(df)
-
+        # Continue with the existing code...
+        #stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
+        #stream_producer.timeseries.buffer.publish(df)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        print("Stopping app to prevent data loss..")
+        os.kill(os.getpid(), signal.SIGTERM)
+        
 
 # Handle event data from samples that emit event data
 def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
@@ -59,3 +67,4 @@ print("Listening to streams. Press CTRL-C to exit.")
 
 # Handle termination signals and provide a graceful exit
 qx.App.run()
+print("Goodbye!")
