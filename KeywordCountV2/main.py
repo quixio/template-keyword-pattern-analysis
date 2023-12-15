6fb03553f67b261a82ec5fed66c1efee8285173a
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-15 09:16:11 +0000

    Edited 'main.py'

--- KeywordCountV2/main.py
+++ KeywordCountV2/main.py
@@ -1,22 +1,41 @@
 import quixstreams as qx
 import os
 import pandas as pd
-
+from collections import defaultdict
+from datetime import datetime, timedelta
+import ast
 
 client = qx.QuixStreamingClient()
 
 topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "empty-transformation")
 topic_producer = client.get_topic_producer(os.environ["output"])
 
+# Initialize a dictionary to store keyword counts and timestamps
+keyword_counts = defaultdict(lambda: defaultdict(list))
 
 def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
+    # Extract keywords and timestamp from the DataFrame
+    keywords = ast.literal_eval(df['StringValues']['extracted_keywords'][0])
+    timestamp = pd.to_datetime(df['Timestamps'][0], unit='ns')
 
-    # Transform data frame here in this method. You can filter data or add new features.
-    # Pass modified data frame to output stream using stream producer.
-    # Set the output stream id to the same as the input stream or change it,
-    # if you grouped or merged data with different key.
-    stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-    stream_producer.timeseries.buffer.publish(df)
+    # Update keyword counts and timestamps
+    for keyword, _ in keywords:
+        keyword_counts[keyword]['timestamps'].append(timestamp)
+        #keyword_counts[keyword]['counts'].append(len(keyword_counts[keyword]['timestamps']))
+
+    # Calculate and print keyword counts in the specified time periods
+    for keyword, data in keyword_counts.items():
+        timestamps = data['timestamps']
+        #counts = data['counts']
+
+        for period in [1, 15, 60]:  # Time periods in minutes
+            period_start = timestamp - timedelta(minutes=period)
+            count = sum(t >= period_start for t in timestamps)
+            print(f"Count of '{keyword}' in the last {period} minutes: {count}")
+
+    # Continue with the existing code...
+    #stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
+    #stream_producer.timeseries.buffer.publish(df)
 
 
 # Handle event data from samples that emit event data
