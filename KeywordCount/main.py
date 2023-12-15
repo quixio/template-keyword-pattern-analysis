6080b7903bbd7bf069a984bfe4eb4743d453c62a
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-15 11:16:19 +0000

    Edited 'main.py'

--- KeywordCount/main.py
+++ KeywordCount/main.py
@@ -16,59 +16,9 @@ def expand_keywords(row: dict):
     new_rows['Timestamp'] = row['Timestamp']
     return new_rows
 
-def time_delta_check(counts, key, state, current_time, window_start, delta_seconds):
-    if current_time - window_start > timedelta(seconds=delta_seconds):
-        return_data = counts[key]
-        return_data["period"] = f"{delta_seconds} seconds"
-        counts[key] = return_data
+def sum_keywords(row: dict, state: State, some_param):
 
-
-        print(f"Clearing state for {key}")
-        counts[key] = {}
-        state.set("counts", counts)
-        state.set("window_start", current_time.isoformat())
-
-        return counts
-
-clear_state = True
-def sum_keywords(row: dict, state: State, a):
-    global clear_state
-
-    if clear_state:
-        print("Initializing state")
-
-        state.set("counts", {"one_minute_data": {}, "15_minute_data": {}})
-        state.set("window_start", datetime.fromtimestamp(row['Timestamp'] / 1e9).isoformat())
-        clear_state = False
-
-    counts = state.get("counts", {})
-    window_start = datetime.fromisoformat(state.get("window_start"))
-
-    current_time = datetime.fromtimestamp(row['Timestamp'] / 1e9)
-
-    for key in row:
-        if key == "Timestamp":
-            continue
-
-        for period in counts:
-            print (period)
-            if period not in counts:
-                counts[period] = {}
-            
-            counts[period][key] = counts[period].get(key, 0) + 1
-
-    state.set('counts', counts)
-
-    #print("********************")
-    #for p in counts:
-    #    print(p)
-    #print("********************")
-
-    return_data = {}
-    return_data = time_delta_check(counts, "one_minute_data", state, current_time, window_start, 60)
-    #return_data = time_delta_check(counts, "15_minute_data", state, current_time, window_start, 900)
-
-    return return_data
+    return row
 
 def sdf_way():
     sdf = app.dataframe(input_topic)
