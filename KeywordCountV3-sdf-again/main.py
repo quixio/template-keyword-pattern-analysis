0dbc50a0e221f3f5ff2ed3b045b0900fee7eda9d
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-15 11:59:18 +0000

    Edited 'main.py'

--- KeywordCountV3-sdf-again/main.py
+++ KeywordCountV3-sdf-again/main.py
@@ -1,85 +1,78 @@
-import quixstreams as qx
+from quixstreams import Application, State
+from quixstreams.models.serializers.quix import QuixDeserializer, QuixTimeseriesSerializer, JSONSerializer
 import os
-import pandas as pd
-from collections import defaultdict
-from datetime import datetime, timedelta
+import time
 import ast
-import signal
-import traceback
+from datetime import datetime, timedelta
+from quixstreams.kafka import Producer
 
 
-client = qx.QuixStreamingClient()
+app = Application.Quix("keywords-3", auto_offset_reset="earliest")
+input_topic = app.topic(os.environ["input"], value_deserializer=QuixDeserializer())
+output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer())
 
-topic_consumer = client.get_topic_consumer(os.environ["input"], consumer_group = "empty-transformation")
-topic_producer = client.get_topic_producer(os.environ["output"])
-
-# Initialize a dictionary to store keyword counts and timestamps
-keyword_counts = defaultdict(lambda: defaultdict(list))
-
-def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
-    
-    if 'extracted_keywords' not in df:
-        print('extracted_keywords not found in dataframe')
-    else:
-        try:
-            # Extract keywords and timestamp from the DataFrame
-            keywords = ast.literal_eval(df['extracted_keywords'][0])
-            print(df.columns)
-            timestamp = pd.to_datetime(df['timestamp'][0], unit='ns')
-
-            # Update keyword counts and timestamps
-            for keyword, _ in keywords:
-                keyword_counts[keyword]['timestamps'].append(timestamp)
-                #keyword_counts[keyword]['counts'].append(len(keyword_counts[keyword]['timestamps']))
-
-            # Calculate keyword counts in the specified time periods and store in a DataFrame
-            data = []
-            for keyword, timestamps in keyword_counts.items():
-                row = {'Keyword': keyword}
-                for period in [1, 15, 60]:  # Time periods in minutes
-                    period_start = timestamp - timedelta(minutes=period)
-                    count = sum(t >= period_start for t in timestamps)
-                    row[f'{period}_min_count'] = count
-                data.append(row)
-            count_df = pd.DataFrame(data)
-            #print(count_df)
-            output_stream = topic_producer.get_or_create_stream("a")
-            output_stream.timeseries.publish(count_df)
-
-            # Continue with the existing code...
-            #stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-            #stream_producer.timeseries.buffer.publish(df)
-        except Exception as e:
-            print("An error occurred:")
-            traceback.print_exc()
-            print(" ")
-            print("-----------------------------------------------------------------------")
-            print("Disconnecting dataframe handler and stopping app to prevent data loss..")
-        
-            stream_consumer.events.on_data_received = None
-            stream_consumer.timeseries.on_dataframe_received = None
-            os.kill(os.getpid(), signal.SIGTERM)
-        
-
-# Handle event data from samples that emit event data
-def on_event_data_received_handler(stream_consumer: qx.StreamConsumer, data: qx.EventData):
-    print(data)
-    # handle your event data here
+def expand_keywords(row: dict):
+    new_rows = row['extracted_keywords']
+    new_rows['Timestamp'] = row['Timestamp']
+    return new_rows
 
 
-def on_stream_received_handler(stream_consumer: qx.StreamConsumer):
-    # subscribe to new DataFrames being received
-    # if you aren't familiar with DataFrames there are other callbacks available
-    # refer to the docs here: https://docs.quix.io/sdk/subscribe.html
-    stream_consumer.events.on_data_received = on_event_data_received_handler # register the event data callback
-    stream_consumer.timeseries.on_dataframe_received = on_dataframe_received_handler
+def sum_keywords(row: dict, state: State, some_param):
+    # Initialize state if it doesn't exist
+    counts = state.get("counts", {
+        "1min": {},
+        "15min": {},
+        "60min": {}
+    })
 
+    # Get current timestamp
+    current_timestamp = datetime.fromtimestamp(row['Timestamp'] / 1e9)
 
-# subscribe to new streams being received
-topic_consumer.on_stream_received = on_stream_received_handler
+    # Update counts
+    for keyword, count in row.items():
+        if keyword != 'Timestamp':
+            for window, window_counts in counts.items():
+                # Calculate window start time
+                if window == "1min":
+                    window_start = current_timestamp - timedelta(minutes=1)
+                elif window == "15min":
+                    window_start = current_timestamp - timedelta(minutes=15)
+                elif window == "60min":
+                    window_start = current_timestamp - timedelta(minutes=60)
 
-print("Listening to streams. Press CTRL-C to exit.")
+                # Remove counts outside of window
+                keys_to_remove = []
+                for timestamp_str, keyword_counts in window_counts.items():
+                    if datetime.fromtimestamp(float(timestamp_str)) < window_start:
+                        keys_to_remove.append(timestamp_str)
 
-# Handle termination signals and provide a graceful exit
-qx.App.run()
-print("Goodbye!")
+                for key in keys_to_remove:
+                    del window_counts[key]
+
+                # Add new count
+                timestamp_str = str(current_timestamp.timestamp())
+                if timestamp_str not in window_counts:
+                    window_counts[timestamp_str] = {}
+                if keyword not in window_counts[timestamp_str]:
+                    window_counts[timestamp_str][keyword] = 0
+                window_counts[timestamp_str][keyword] += count
+
+    print(counts)  # Debug print
+    state.set("counts", counts)
+    return counts
+
+def sdf_way():
+    sdf = app.dataframe(input_topic)
+    sdf = sdf[sdf.contains('extracted_keywords')]
+    sdf = sdf[sdf['extracted_keywords'].notnull()]
+    sdf['extracted_keywords'] = sdf['extracted_keywords'].apply(lambda value: dict(ast.literal_eval(value)))
+    sdf = sdf.apply(expand_keywords)
+    #sdf = sdf.apply(sum_keywords, stateful=True)
+    sdf = sdf.apply(lambda row, state: sum_keywords(row, state, "thing"), stateful=True)
+    sdf = sdf.to_topic(output_topic)
+    return sdf
+
+sdf = sdf_way()
+
+if __name__ == "__main__":
+    app.run(sdf)
