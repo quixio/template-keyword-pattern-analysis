5942a8b10c54973bf4de708e43c466cefd307f57
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-15 09:36:32 +0000

    Edited 'main.py'

--- KeywordCountV2/main.py
+++ KeywordCountV2/main.py
@@ -17,45 +17,39 @@ keyword_counts = defaultdict(lambda: defaultdict(list))
 
 def on_dataframe_received_handler(stream_consumer: qx.StreamConsumer, df: pd.DataFrame):
     
-    if 'extracted_keywords' in df:
-        print("!!!!!!!!! string values")
-        print(df.columns)
+    if 'extracted_keywords' not in df:
+        print('extracted_keywords not found in dataframe')
     else:
-        print("no string values")
-        print(df.columns)
-    
-    return
+        try:
+            # Extract keywords and timestamp from the DataFrame
+            keywords = ast.literal_eval(df['extracted_keywords'][0])
+            timestamp = pd.to_datetime(df['Timestamps'][0], unit='ns')
 
-    try:
-        # Extract keywords and timestamp from the DataFrame
-        keywords = ast.literal_eval(df['StringValues']['extracted_keywords'][0])
-        timestamp = pd.to_datetime(df['Timestamps'][0], unit='ns')
+            # Update keyword counts and timestamps
+            for keyword, _ in keywords:
+                keyword_counts[keyword]['timestamps'].append(timestamp)
+                #keyword_counts[keyword]['counts'].append(len(keyword_counts[keyword]['timestamps']))
 
-        # Update keyword counts and timestamps
-        for keyword, _ in keywords:
-            keyword_counts[keyword]['timestamps'].append(timestamp)
-            #keyword_counts[keyword]['counts'].append(len(keyword_counts[keyword]['timestamps']))
+            # Calculate and print keyword counts in the specified time periods
+            for keyword, data in keyword_counts.items():
+                timestamps = data['timestamps']
+                #counts = data['counts']
 
-        # Calculate and print keyword counts in the specified time periods
-        for keyword, data in keyword_counts.items():
-            timestamps = data['timestamps']
-            #counts = data['counts']
+                for period in [1, 15, 60]:  # Time periods in minutes
+                    period_start = timestamp - timedelta(minutes=period)
+                    count = sum(t >= period_start for t in timestamps)
+                    print(f"Count of '{keyword}' in the last {period} minutes: {count}")
 
-            for period in [1, 15, 60]:  # Time periods in minutes
-                period_start = timestamp - timedelta(minutes=period)
-                count = sum(t >= period_start for t in timestamps)
-                print(f"Count of '{keyword}' in the last {period} minutes: {count}")
-
-        # Continue with the existing code...
-        #stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
-        #stream_producer.timeseries.buffer.publish(df)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        print("Disconnecting dataframe handler and stopping app to prevent data loss..")
-    
-        stream_consumer.events.on_data_received = None
-        stream_consumer.timeseries.on_dataframe_received = None
-        os.kill(os.getpid(), signal.SIGTERM)
+            # Continue with the existing code...
+            #stream_producer = topic_producer.get_or_create_stream(stream_id = stream_consumer.stream_id)
+            #stream_producer.timeseries.buffer.publish(df)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            print("Disconnecting dataframe handler and stopping app to prevent data loss..")
+        
+            stream_consumer.events.on_data_received = None
+            stream_consumer.timeseries.on_dataframe_received = None
+            os.kill(os.getpid(), signal.SIGTERM)
         
 
 # Handle event data from samples that emit event data
