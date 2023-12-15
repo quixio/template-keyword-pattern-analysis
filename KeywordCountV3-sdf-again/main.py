abc78c2f831529e38a29abf31e64ec38cae00e69
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-15 14:46:14 +0000

    Edited 'main.py'

--- KeywordCountV3-sdf-again/main.py
+++ KeywordCountV3-sdf-again/main.py
@@ -53,8 +53,9 @@ def sum_keywords_tumbling(row: dict, state: State, some_param):
                         ended_windows[window_start_str] = {}
                     ended_windows[window_start_str][keyword] = sum(window_counts[keyword].values())
                     window_counts[keyword] = {}
-                    
-                #print(f"Updated counts for keyword {keyword} in window ({window_length}) starting at {window_start_str}: {window_counts[keyword]}")  # Debug print
+
+                if window_length == 1:    
+                    print(f"Updated counts for keyword {keyword} in window ({window_length}) starting at {window_start_str}: {window_counts[keyword]}")  # Debug print
 
     # Debug print
     #print({window_start: {keyword: sum(times.values()) for keyword, times in counts[window_start].items()} for window_start in counts}) 
