455cb86b28b57c3bd9259b0069855d5303c55d36
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-15 11:25:45 +0000

    Edited 'main.py'

--- KeywordCount/main.py
+++ KeywordCount/main.py
@@ -17,8 +17,47 @@ def expand_keywords(row: dict):
     return new_rows
 
 def sum_keywords(row: dict, state: State, some_param):
-    print(row)
-    return row
+    # Initialize state if it doesn't exist
+    if not state.exists("counts"):
+        state["counts"] = {
+            "1min": {},
+            "15min": {},
+            "60min": {}
+        }
+
+    # Get current timestamp
+    current_timestamp = datetime.fromtimestamp(row['Timestamp'] / 1e9)
+
+    # Update counts
+    for keyword, count in row.items():
+        if keyword != 'Timestamp':
+            for window, counts in state["counts"].items():
+                # Calculate window start time
+                if window == "1min":
+                    window_start = current_timestamp - timedelta(minutes=1)
+                elif window == "15min":
+                    window_start = current_timestamp - timedelta(minutes=15)
+                elif window == "60min":
+                    window_start = current_timestamp - timedelta(minutes=60)
+
+                # Remove counts outside of window
+                keys_to_remove = []
+                for timestamp, keyword_counts in counts.items():
+                    if timestamp < window_start:
+                        keys_to_remove.append(timestamp)
+
+                for key in keys_to_remove:
+                    del counts[key]
+
+                # Add new count
+                if current_timestamp not in counts:
+                    counts[current_timestamp] = {}
+                if keyword not in counts[current_timestamp]:
+                    counts[current_timestamp][keyword] = 0
+                counts[current_timestamp][keyword] += count
+
+    print(state["counts"])  # Debug print
+    return state["counts"]
 
 def sdf_way():
     sdf = app.dataframe(input_topic)
