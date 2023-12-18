93e352b1cf9c31fe099eb944c88cef572d5316c2
Steve Rosam <srosam+quix-demos@gmail.com>
2023-12-18 13:22:49 +0000

    Edited 'main.py'

--- KeywordCountV3-sdf-again/main.py
+++ KeywordCountV3-sdf-again/main.py
@@ -35,7 +35,7 @@ def sum_keywords_tumbling(row: dict, state: State, some_param):
                                                             microseconds=current_timestamp.microsecond)
 
                 window_start_str = str(window_start.timestamp())
-
+                print(window_start_str)
                 if window_start_str not in counts:
                     counts[window_start_str] = {}
 
